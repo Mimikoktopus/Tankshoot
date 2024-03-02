f6e02aa9a507ed6be78cc353b3310acddb4d6c0b
Felix <felised@gmx.de>
2024-03-02 12:43:21 +0100

    1.12

--- shooter.py
+++ shooter.py
@@ -20,6 +20,7 @@ window     = pygame.display.set_mode((infoObject.current_w, infoObject.current_h
 PlayerX    = infoObject.current_w /2
 PlayerY    = infoObject.current_h /2
 PlayerR    = 0
+Punkte     = 0
 KugelX     = PlayerX
 KugelY     = PlayerY
 KugelXG    = 4
@@ -34,13 +35,17 @@ Gameover   = pygame.image.load("Gameover.jpg")
 FeindB     = pygame.image.load("Feind.png")
 Panzer     = pygame.image.load("PanzerUntersatz.png")
 PanzerK    = pygame.image.load("PanzerKanonenturm.png")
+Wonp       = pygame.image.load("win.jpg")
 Schuss     = pygame.mixer.Sound("Schuss.mp3")
 TodG       = pygame.mixer.Sound("TodG.mp3")
 TodS       = pygame.mixer.Sound("TodS.mp3")
 Music      = pygame.mixer.music.load("Music.mp3")
+
 pygame.mixer.music.play(-1)
 Hintergrund= pygame.transform.scale(Hintergrund,(infoObject.current_w, infoObject.current_h))
 Gameover   = pygame.transform.scale(Gameover,(infoObject.current_w, infoObject.current_h))
+Wonp       = pygame.transform.scale(Wonp,(infoObject.current_w, infoObject.current_h))
+
 
 #Funktions Blog
 def Feindreturn():
@@ -68,123 +73,13 @@ pygame.mouse.set_visible(0)
 spielaktiv = True
 Kugelaktiv = False
 GameoverS  = False
+Win        = False
 
 while spielaktiv:
-    if GameoverS == False:
-        window.fill(GRAU)
-        MausP = pygame.mouse.get_pos()
-        window.blit(Hintergrund, pygame.Rect(0, 0, 32, 32))
-        if Kugelaktiv:
-            pygame.draw.circle(window, (ORANGE), (KugelX, KugelY), 5)
-            KugelX  = KugelX + KugelXG
-            KugelY  = KugelY + KugelYG
-        
-        Soldatw =pygame.transform.rotate(Panzer, PlayerR)
-        window.blit(Soldatw, (PlayerX - Soldatw.get_width()/2, PlayerY - Soldatw.get_height()/2))
-        
-        Px = MausP[0] - PlayerX
-        Py = MausP[1] - PlayerY
-        Soldatw2 = math.degrees (math.atan2 (-Py,Px))
-        Soldatw =pygame.transform.rotate(PanzerK, Soldatw2)
-        window.blit(Soldatw, (PlayerX - Soldatw.get_width()/2, PlayerY - Soldatw.get_height()/2))
-        
-        for Feind in Feinde:
-            Fx = PlayerX - Feind[0]
-            Fy = PlayerY - Feind[1]
-            Feindw2 = math.degrees (math.atan2 (-Fy,Fx))
-            Feindw =pygame.transform.rotate(FeindB, Feindw2)
-            window.blit(Feindw, (Feind[0] - FeindB.get_width()/2, Feind[1] - FeindB.get_height()/2))
-
-        
-        FadenkreuzP = pygame.Rect(MausP[0]-16, MausP[1]-16, 32, 32)
-        window.blit(Fadenkreuz, FadenkreuzP)
-        #rect = pygame.Rect(infoObject.current_w /4, infoObject.current_h /4, infoObject.current_w /2, 50)
-        #pygame.draw.rect(window, (SCHWARZ), rect)
-        pygame.display.flip()
-        clock.tick(60)
-
-        for Feind in Feinde:  
-            FeindX = Feind[0]
-            FeindY = Feind[1]
-            FeindXG = (FeindX-PlayerX)/100
-            FeindYG = (FeindY-PlayerY)/100  
-            divisor = abs(FeindXG)
-            if abs(FeindYG) > divisor:
-                divisor = abs(FeindYG)
-            FeindYG  = FeindYG/divisor
-            FeindXG  = FeindXG/divisor  
-            FeindYG  = FeindYG * -5
-            FeindXG  = FeindXG * -5
-            Feind[0]   = FeindX + FeindXG
-            Feind[1]   = FeindY + FeindYG
-
-        if Kugelaktiv :
-            for i in range(len(Feinde)):
-                Feind   = Feinde[i]   
-                FeindX  = Feind[0]
-                FeindY  = Feind[1]
-                FeindXA = FeindX - KugelX
-                FeindYA = FeindY - KugelY
-                FeindA  = math.sqrt(FeindXA ** 2 + FeindYA ** 2)
-                if FeindA < 35 :
-                    del Feinde[i]
-                    Feindreturn()   # Feind Hitbox
-                    pygame.mixer.Sound.play(TodG)
-                    Kugelaktiv = False
-        FeindXA = FeindX - PlayerX
-        FeindYA = FeindY - PlayerY
-        FeindA  = math.sqrt(FeindXA ** 2 + FeindYA ** 2)
-        if FeindA < 80:
-            pygame.mixer.Sound.play(TodS)
-            GameoverS = True    # Player Hitbox
-        if KugelX < 0 :
-            Kugelaktiv = False
-        if KugelY < 0 :
-            Kugelaktiv = False
-        if KugelX > infoObject.current_w :
-            Kugelaktiv = False
-        if KugelY > infoObject.current_h :
-            Kugelaktiv = False    
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                spielaktiv = False 
-        keys  = pygame.key.get_pressed()
-        mouse = pygame.mouse.get_pressed()
-        if keys[pygame.K_w]:
-            PlayerX += math.cos(math.radians(-PlayerR))*7
-            PlayerY += math.sin(math.radians(-PlayerR))*7
-        if keys[pygame.K_s]:
-            PlayerX += math.cos(math.radians(-PlayerR))*-7
-            PlayerY += math.sin(math.radians(-PlayerR))*-7
-        if keys[pygame.K_a]:
-            PlayerR = PlayerR +3
-        if keys[pygame.K_d]:
-            PlayerR = PlayerR -3
-        if keys[pygame.K_ESCAPE]:
-            spielaktiv = False
-        if mouse[0] and (not Kugelaktiv):
-            Kugelaktiv = True
-            pygame.mixer.Sound.play(Schuss)
-            KugelX    = PlayerX
-            KugelY    = PlayerY
-            KugelXG = (MausP[0]-PlayerX)/100
-            KugelYG = (MausP[1]-PlayerY)/100  
-            divisor = abs(KugelXG)
-            if abs(KugelYG) > divisor:
-                divisor = abs(KugelYG)
-            KugelYG = KugelYG/divisor
-            KugelXG = KugelXG/divisor  
-            KugelYG = KugelYG * 10
-            KugelXG = KugelXG * 10 
-        if PlayerX < 20 :
-            PlayerX = 20
-        if PlayerY < 20 :
-            PlayerY = 20
-        if PlayerX > infoObject.current_w -20 :
-            PlayerX = infoObject.current_w -20
-        if PlayerY > infoObject.current_h -20 :
-            PlayerY = infoObject.current_h-20
-    else:
+    if Punkte > 20:
+        Win = True
+    
+    if Win == True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 spielaktiv = False
@@ -193,12 +88,148 @@ while spielaktiv:
         window.blit(Fadenkreuz, FadenkreuzP)
         pygame.display.flip()
         clock.tick(60)
-        window.blit(Gameover, pygame.Rect(0, 0, infoObject.current_h, infoObject.current_w))
+        window.blit(Wonp, pygame.Rect(0, 0, infoObject.current_h, infoObject.current_w))
         keys  = pygame.key.get_pressed()
         if keys[pygame.K_ESCAPE]:
             spielaktiv = False 
         if keys[pygame.K_RETURN]:
+            Punkte = 0
             Feinde = [[30, 30]]
             PlayerX = infoObject.current_w /2
             PlayerY = infoObject.current_h /2
-            GameoverS = False
+            Win = False
+    else:
+        if GameoverS == False:
+            window.fill(GRAU)
+            MausP = pygame.mouse.get_pos()
+            window.blit(Hintergrund, pygame.Rect(0, 0, 32, 32))
+            if Kugelaktiv:
+                pygame.draw.circle(window, (ORANGE), (KugelX, KugelY), 5)
+                KugelX  = KugelX + KugelXG
+                KugelY  = KugelY + KugelYG
+            
+            Soldatw =pygame.transform.rotate(Panzer, PlayerR)
+            window.blit(Soldatw, (PlayerX - Soldatw.get_width()/2, PlayerY - Soldatw.get_height()/2))
+            
+            Px = MausP[0] - PlayerX
+            Py = MausP[1] - PlayerY
+            Soldatw2 = math.degrees (math.atan2 (-Py,Px))
+            Soldatw =pygame.transform.rotate(PanzerK, Soldatw2)
+            window.blit(Soldatw, (PlayerX - Soldatw.get_width()/2, PlayerY - Soldatw.get_height()/2))
+            
+            for Feind in Feinde:
+                Fx = PlayerX - Feind[0]
+                Fy = PlayerY - Feind[1]
+                Feindw2 = math.degrees (math.atan2 (-Fy,Fx))
+                Feindw =pygame.transform.rotate(FeindB, Feindw2)
+                window.blit(Feindw, (Feind[0] - FeindB.get_width()/2, Feind[1] - FeindB.get_height()/2))
+
+            
+            FadenkreuzP = pygame.Rect(MausP[0]-16, MausP[1]-16, 32, 32)
+            window.blit(Fadenkreuz, FadenkreuzP)
+            #rect = pygame.Rect(infoObject.current_w /4, infoObject.current_h /4, infoObject.current_w /2, 50)
+            #pygame.draw.rect(window, (SCHWARZ), rect)
+            pygame.display.flip()
+            clock.tick(60)
+
+            for Feind in Feinde:  
+                FeindX = Feind[0]
+                FeindY = Feind[1]
+                FeindXG = (FeindX-PlayerX)/100
+                FeindYG = (FeindY-PlayerY)/100  
+                divisor = abs(FeindXG)
+                if abs(FeindYG) > divisor:
+                    divisor = abs(FeindYG)
+                FeindYG  = FeindYG/divisor
+                FeindXG  = FeindXG/divisor  
+                FeindYG  = FeindYG * -5
+                FeindXG  = FeindXG * -5
+                Feind[0]   = FeindX + FeindXG
+                Feind[1]   = FeindY + FeindYG
+
+            if Kugelaktiv :
+                for i in range(len(Feinde)):
+                    Feind   = Feinde[i]   
+                    FeindX  = Feind[0]
+                    FeindY  = Feind[1]
+                    FeindXA = FeindX - KugelX
+                    FeindYA = FeindY - KugelY
+                    FeindA  = math.sqrt(FeindXA ** 2 + FeindYA ** 2)
+                    if FeindA < 35 :
+                        del Feinde[i]
+                        Feindreturn()   # Feind Hitbox
+                        Punkte += 1
+                        pygame.mixer.Sound.play(TodG)
+                        Kugelaktiv = False
+            FeindXA = FeindX - PlayerX
+            FeindYA = FeindY - PlayerY
+            FeindA  = math.sqrt(FeindXA ** 2 + FeindYA ** 2)
+            if FeindA < 80:
+                pygame.mixer.Sound.play(TodS)
+                GameoverS = True    # Player Hitbox
+            if KugelX < 0 :
+                Kugelaktiv = False
+            if KugelY < 0 :
+                Kugelaktiv = False
+            if KugelX > infoObject.current_w :
+                Kugelaktiv = False
+            if KugelY > infoObject.current_h :
+                Kugelaktiv = False    
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    spielaktiv = False 
+            keys  = pygame.key.get_pressed()
+            mouse = pygame.mouse.get_pressed()
+            if keys[pygame.K_w]:
+                PlayerX += math.cos(math.radians(-PlayerR))*7
+                PlayerY += math.sin(math.radians(-PlayerR))*7
+            if keys[pygame.K_s]:
+                PlayerX += math.cos(math.radians(-PlayerR))*-7
+                PlayerY += math.sin(math.radians(-PlayerR))*-7
+            if keys[pygame.K_a]:
+                PlayerR = PlayerR +3
+            if keys[pygame.K_d]:
+                PlayerR = PlayerR -3
+            if keys[pygame.K_ESCAPE]:
+                spielaktiv = False
+            if mouse[0] and (not Kugelaktiv):
+                Kugelaktiv = True
+                pygame.mixer.Sound.play(Schuss)
+                KugelX    = PlayerX
+                KugelY    = PlayerY
+                KugelXG = (MausP[0]-PlayerX)/100
+                KugelYG = (MausP[1]-PlayerY)/100  
+                divisor = abs(KugelXG)
+                if abs(KugelYG) > divisor:
+                    divisor = abs(KugelYG)
+                KugelYG = KugelYG/divisor
+                KugelXG = KugelXG/divisor  
+                KugelYG = KugelYG * 10
+                KugelXG = KugelXG * 10 
+            if PlayerX < 20 :
+                PlayerX = 20
+            if PlayerY < 20 :
+                PlayerY = 20
+            if PlayerX > infoObject.current_w -20 :
+                PlayerX = infoObject.current_w -20
+            if PlayerY > infoObject.current_h -20 :
+                PlayerY = infoObject.current_h-20
+        else:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    spielaktiv = False
+            Punkte = 0
+            MausP = pygame.mouse.get_pos()
+            FadenkreuzP = pygame.Rect(MausP[0]-16, MausP[1]-16, 32, 32)
+            window.blit(Fadenkreuz, FadenkreuzP)
+            pygame.display.flip()
+            clock.tick(60)
+            window.blit(Gameover, pygame.Rect(0, 0, infoObject.current_h, infoObject.current_w))
+            keys  = pygame.key.get_pressed()
+            if keys[pygame.K_ESCAPE]:
+                spielaktiv = False 
+            if keys[pygame.K_RETURN]:
+                Feinde = [[30, 30]]
+                PlayerX = infoObject.current_w /2
+                PlayerY = infoObject.current_h /2
+                GameoverS = False
